71a8db7b67d4bb8d62914ed6ccca65ad92a63e5c
hatelove <jay.hatelove@gmail.com>
2017-03-23 11:16:59 +0800

    add the first failed test case: s length is 1, should return true

---
+++ codewars_PrimeString/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace codewars_PrimeString
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void s_is_b_should_be_prime_string()
+        {
+            var s = "b";
+            Assert.IsTrue(new Kata().PrimeString(s));
+        }
+    }
+
+    public class Kata
+    {
+        public bool PrimeString(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
